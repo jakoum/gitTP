35aa64ac751d280519cb5ea6f92233bd90051972
hafida <hafida97jak@gmail.com>
2022-02-11 11:39:40 +0100

    ok

--- src/main/java/ServeurJaxWS.java
+++ src/main/java/ServeurJaxWS.java
@@ -6,5 +6,6 @@ public class ServeurJaxWS {
 	public static void main(String[] args) {
         String url = "http://localhost:8585/";
         Endpoint.publish(url , new BanqueService());
+        System.out.print("first test ok ");
     }
 }
